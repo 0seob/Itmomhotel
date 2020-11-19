d4f9f2731a68e40e2710c36b52f59545dea7b248
EunJeLee <dldmswp3@naver.com>
2020-11-19 21:34:24 +0900

    login success

--- app.py
+++ app.py
@@ -1,90 +1,16 @@
-from flask import Flask, render_template, request, redirect ,url_for
+from flask import Flask, render_template, request, redirect ,url_for, flash, session
+from flask_cors import CORS
 import pymysql
 
+DEBUG = True
 app = Flask(__name__)
+# enable CORS
+app.config.from_object(__name__)
+app.secret_key = 'some_secret'
+CORS(app)
 
-@app.route('/home')
-def home():
-    return render_template('manager/index.html')
 
-
-@app.route('/login', methods=['post', 'get'])
-def login():
-    error = None
-    if request.method == 'POST':
-        
-        email = request.form['email']
-        pw = request.form['password']
-
-        print(email,pw)
-        
-        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
-        cursor = conn.cursor()
-         
-        query = "SELECT username FROM userinfo WHERE email = %s AND password = %s"
-        value = (email, pw)
-        cursor.execute("set names utf8")
-        cursor.execute(query, value)
-        data = (cursor.fetchall())
-        
-        cursor.close()
-        conn.close()
-        
-        for row in data:
-            data = row[0]
-        
-        if data:
-            print ('login success')
-            return redirect(url_for('home', username=data))
-        else:
-            error = 'Invalid input data detected!'
-            
-        #return redirect(url_for('success', name=user))
-    
-    return render_template('manager/login.html', error=error)
-@app.route('/register', methods=['post', 'get'])
-def regist():
-    error = None
-    if request.method == 'POST':
-        
-        name = request.form['username']
-        email = request.form['email']
-        pw = request.form['password']
-        
-        print (name, email, pw)
-        
-        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
-        cursor = conn.cursor()
-        
-        query = "SELECT 1 FROM userinfo WHERE email = '%s' " % (email)
-        #value = (email)
-        cursor.execute(query)
-        data = cursor.fetchall()
-        
-        if data:
-            print ('user other email')
-            error = "The email is already used. please use another one"
-        else:
-            print ('use it okay')
-            query = "INSERT INTO userinfo (username, password, email) values (%s, %s, %s)"
-            value = (name, pw, email)
-            cursor.execute(query, value)
-            data = cursor.fetchall()
-            print (data)
-            if not data:
-                conn.commit()
-                print (data)
-                return "Register Success"
-            else:
-                conn.rollback()
-                print (data)
-                return "Register Failed"
-        
-        
-        cursor.close()
-        conn.close()
-    return render_template('manager/register.html', error=error)
-    
+### Routes for customer part start ###
 @app.route('/')
 @app.route('/index')
 def index():
@@ -117,6 +43,10 @@ def roomdetails():
 @app.route('/rooms')
 def rooms():
     return render_template('customer/rooms.html')
+
+@app.route('/reservationcheck')
+def reservationcheck():
+    return render_template('customer/reservationcheck.html')
 ### Routes for customer part end ###
 
 ### Routes for manager part start ###
@@ -124,65 +54,195 @@ def rooms():
 @app.route('/manager/')
 @app.route('/manager/index')
 def mindex():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/index.html')
 
 @app.route('/manager/charts')
 def mcharts():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/charts.html')
 
 @app.route('/manager/customer')
 def mcustomer():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/customer.html')
 
 @app.route('/manager/employee')
 def memployee():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/employee.html')
 
 @app.route('/manager/login')
 def mlogin():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/login.html')
 
 @app.route('/manager/parking')
 def mparking():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/parking.html')
 
 @app.route('/manager/password')
 def mpassword():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/password.html')
 
 @app.route('/manager/product')
 def mproduct():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/product.html')
 
 @app.route('/manager/profile')
 def mprofile():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/profile.html')
 
 @app.route('/manager/pure')
 def mpure():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/pure.html')
 
 @app.route('/manager/register')
 def mregister():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/register.html')
 
 @app.route('/manager/reservation')
 def mreservation():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/reservation.html')
 
 @app.route('/manager/room')
 def mroom():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/room.html')
 
 @app.route('/manager/survey')
 def msurvey():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/survey.html')
 
 @app.route('/manager/task')
 def mtask():
+    if not session.get('username'):
+        print("sex")
+        return render_template('manager/login.html')
     return render_template('manager/task.html')
+
+@app.route('/home')
+def home():
+    return render_template('manager/index.html')
+
+
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    error = None
+    if request.method == 'POST':
+
+        email = request.form['email']
+        pw = request.form['password']
+
+        print(email,pw)
+
+        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
+        cursor = conn.cursor()
+
+        query = "SELECT username FROM userinfo WHERE email = %s AND password = %s"
+        value = (email, pw)
+        cursor.execute("set names utf8")
+        cursor.execute(query, value)
+        data = (cursor.fetchall())
+
+        cursor.close()
+        conn.close()
+
+        for row in data:
+            data = row[0]
+
+        if data:
+            print ('login success')
+            session['username'] = email
+            return redirect(url_for('mindex'))
+        else:
+            error = 'Invalid input data detected!'
+            flash("Login Failed")
+
+        #return redirect(url_for('success', name=user))
+
+    return render_template('manager/login.html', error=error)
+@app.route('/register', methods=['post', 'get'])
+def regist():
+    error = None
+    if request.method == 'POST':
+
+        name = request.form['username']
+        email = request.form['email']
+        pw = request.form['password']
+
+        print (name, email, pw)
+
+        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
+        cursor = conn.cursor()
+
+        query = "SELECT 1 FROM userinfo WHERE email = '%s' " % (email)
+        #value = (email)
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        if data:
+            print ('user other email')
+            error = "The email is already used. please use another one"
+        else:
+            print ('use it okay')
+            query = "INSERT INTO userinfo (username, password, email) values (%s, %s, %s)"
+            value = (name, pw, email)
+            cursor.execute(query, value)
+            data = cursor.fetchall()
+            print (data)
+            if not data:
+                conn.commit()
+                print (data)
+                return "Register Success"
+            else:
+                conn.rollback()
+                print (data)
+                return "Register Failed"
+
+
+        cursor.close()
+        conn.close()
+    return render_template('manager/register.html', error=error)
+
 ### Routes for manager part end ###
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
