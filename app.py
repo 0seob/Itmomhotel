747679e6df00f5398ec3a908e0d3cbc089fb9812
EunJeLee <dldmswp3@naver.com>
2020-11-24 03:06:04 +0900

    logout and employee add &delete

--- app.py
+++ app.py
@@ -55,111 +55,99 @@ def reservationcheck():
 @app.route('/manager/index')
 def mindex():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/index.html')
 
 @app.route('/manager/charts')
 def mcharts():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/charts.html')
 
 @app.route('/manager/customer')
 def mcustomer():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/customer.html')
 
 @app.route('/manager/employee')
 def memployee():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/employee.html')
 
 @app.route('/manager/login')
 def mlogin():
-    if not session.get('username'):
-        print("sex")
-        return render_template('manager/login.html')
+    session.clear()
     return render_template('manager/login.html')
 
 @app.route('/manager/parking')
 def mparking():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/parking.html')
 
 @app.route('/manager/password')
 def mpassword():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/password.html')
 
 @app.route('/manager/product')
 def mproduct():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/product.html')
 
 @app.route('/manager/profile')
 def mprofile():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/profile.html')
 
 @app.route('/manager/pure')
 def mpure():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/pure.html')
 
 @app.route('/manager/register')
 def mregister():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/register.html')
 
 @app.route('/manager/reservation')
 def mreservation():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/reservation.html')
 
 @app.route('/manager/room')
 def mroom():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/room.html')
 
 @app.route('/manager/survey')
 def msurvey():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/survey.html')
 
 @app.route('/manager/task')
 def mtask():
     if not session.get('username'):
-        print("sex")
         return render_template('manager/login.html')
     return render_template('manager/task.html')
 
 @app.route('/home')
 def home():
+    if not session.get('username'):
+        return render_template('manager/login.html')
     return render_template('manager/index.html')
+
+
 
 
 
@@ -173,7 +161,7 @@ def login():
 
         print(email,pw)
 
-        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
+        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='jmk', charset='utf8')
         cursor = conn.cursor()
 
         query = "SELECT username FROM userinfo WHERE email = %s AND password = %s"
@@ -199,7 +187,7 @@ def login():
         #return redirect(url_for('success', name=user))
 
     return render_template('manager/login.html', error=error)
-@app.route('/register', methods=['post', 'get'])
+@app.route('/manager/register', methods=['post', 'get'])
 def regist():
     error = None
     if request.method == 'POST':
@@ -210,7 +198,7 @@ def regist():
 
         print (name, email, pw)
 
-        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='flask_test', charset='utf8')
+        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='jmk', charset='utf8')
         cursor = conn.cursor()
 
         query = "SELECT 1 FROM userinfo WHERE email = '%s' " % (email)
@@ -241,6 +229,52 @@ def regist():
         cursor.close()
         conn.close()
     return render_template('manager/register.html', error=error)
+
+@app.route('/manager/delete', methods=['post', 'get'])
+def delete():
+    error = None
+    if request.method == 'POST':
+
+        name = request.form['username']
+        email = request.form['email']
+        pw = request.form['password']
+
+        print (name, email, pw)
+
+        conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='jmk', charset='utf8')
+        cursor = conn.cursor()
+
+        query = "SELECT id FROM userinfo WHERE email = '%s' " % (email)
+
+        cursor.execute(query)
+        data = cursor.fetchall()
+        print(data)
+
+        if data:
+            print("start delete")
+            query = "DELETE FROM userinfo where email = %s "
+            value = (email)
+            cursor.execute(query,value)
+            data = cursor.fetchall()
+            print (data)
+            if not data:
+                conn.commit()
+                print (data)
+                return render_template('manager/delete_success.html', error=error)
+            else:
+                conn.rollback()
+                print (data)
+                return render_template('manager/delete_fail.html', error=error)
+        else:
+            print("NO")
+            return render_template('manager/delete_fail.html', error=error)
+
+
+        cursor.close()
+        conn.close()
+    return render_template('/manager/delete.html', error=error)
+
+
 
 ### Routes for manager part end ###
 
